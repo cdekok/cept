5490cdff415771aa6db3e71fd5acf32280230733
Chris de Kok <chris.de.kok@gmail.com>
2013-05-08 22:43:14 +0200

    Rename files and ass composer

--- library/Cept/Conversion/ConverterFactory.php
+++ library/Cept/Conversion/ConverterFactory.php
@@ -11,13 +11,13 @@ class ConverterFactory
      * @return \Cept\Conversion\Converter\ConverterInterface
      * @throws \InvalidArgumentException
      */
-     public static function factory(array $options)
-     {
-         //
-         if($options['writer'] instanceof \Cept\Conversion\Writer\Db) {
+    public static function factory(array $options)
+    {
+        //
+        if ($options['writer'] instanceof \Cept\Conversion\Writer\Db) {
             $converter = new \Cept\Conversion\Converter\Db($options);
             return $converter;
-         }
-         throw new \InvalidArgumentException('No converter available');
-     }
+        }
+        throw new \InvalidArgumentException('No converter available');
+    }
 }
